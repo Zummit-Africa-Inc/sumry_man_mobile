c997a88ec0c9c2ed5acf2b7998a818ddcc1cbe1b
logickoder <jeffrey.orazulike@zummitafrica.com>
2022-08-19 09:22:58 +0100

    merged Lekan branch

--- lib/utils/designs/styles.dart
+++ lib/utils/designs/styles.dart
@@ -40,3 +40,59 @@ final sSignUpTextStyle = GoogleFonts.poppins(
     color: kPrimaryColor,
   ),
 );
+
+final sHeadingTextStyle = GoogleFonts.poppins(
+  textStyle: const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 25,
+    color: kPrimaryColor,
+  ),
+);
+
+final sHeading2TextStyle = GoogleFonts.poppins(
+  textStyle: const TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    color: kTextColorH2,
+  ),
+);
+
+final sSignUpButtonTextStyle = GoogleFonts.poppins(
+  textStyle: const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+    color: kWhite,
+  ),
+);
+
+final sTextTextStyle = GoogleFonts.poppins(
+  textStyle: const TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    color: kPrimaryColor,
+  ),
+);
+
+final sText2TextStyle = GoogleFonts.poppins(
+  textStyle: const TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    color: kTextColorLink,
+  ),
+);
+
+final sOrTextStyle = GoogleFonts.inter(
+  textStyle: const TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 9.92,
+    color: kBlack,
+  ),
+);
+
+final sFacebookButtonTextStyle = GoogleFonts.poppins(
+  textStyle: const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+    color: kPrimaryColor,
+  ),
+);
