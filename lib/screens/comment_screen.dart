ef5c89bfe8efa9a59be0f49eea2bb99f357ed261
logickoder <jeffrey.orazulike@zummitafrica.com>
2022-09-20 13:23:30 +0100

    updated comments implementation

--- lib/screens/comment_screen.dart
+++ lib/screens/comment_screen.dart
@@ -1,6 +1,6 @@
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sumry_man/utils/extensions.dart';
 
 import '../components/app_bar.dart';
 import '../components/buttons.dart';
@@ -9,46 +9,39 @@ import '../components/drawer.dart';
 import '../components/images.dart';
 import '../components/input_field.dart';
 import '../components/spacers.dart';
+import '../data/repository/comment_repository.dart';
 import '../utils/designs/dimens.dart';
 import '../utils/res/res_profile.dart';
 import '../data/repository/user_repository.dart';
 
-
-
 class CommentScreen extends ConsumerStatefulWidget {
-   const CommentScreen({Key? key}) : super(key: key);
+  const CommentScreen({Key? key}) : super(key: key);
 
   @override
   ConsumerState<CommentScreen> createState() => _CommentScreenState();
 }
 
 class _CommentScreenState extends ConsumerState<CommentScreen> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _commentController = TextEditingController();
+  var _initialized = false;
+  var _isLoading = false;
 
-   TextEditingController nameController = TextEditingController();
-
-   TextEditingController emailController = TextEditingController();
-
-   TextEditingController commentController = TextEditingController();
-
-  final databaseRef = FirebaseDatabase.instance.reference();
-
+  @override
+  void didChangeDependencies() {
+    if (!_initialized) {
+      final user = ref.watch(userRepository);
+      _nameController.text = user?.displayName ?? '';
+      _emailController.text = user?.email ?? '';
+      _initialized = true;
+    }
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    final repo = ref.read(userRepository.notifier);
-    final user = ref.watch(userRepository);
-
     final theme = Theme.of(context);
-
-    if(repo.authenticated){
-      setState(() {
-        nameController.text = user?.displayName as String;
-        emailController.text = user?.email as String;
-
-      });
-    }
-
 
     return Scaffold(
       drawer: const AppDrawer(),
@@ -76,14 +69,14 @@ class _CommentScreenState extends ConsumerState<CommentScreen> {
             vSpace(sPadding + sPadding / 2),
             InputField(
               state: InputFieldState(
-                controller: nameController,
+                controller: _nameController,
                 label: ResCommentScreen.fullName,
               ),
             ),
             vSpace(sPadding),
             InputField(
               state: InputFieldState(
-                controller: emailController,
+                controller: _emailController,
                 label: ResCommentScreen.emailAddress,
               ),
             ),
@@ -92,7 +85,7 @@ class _CommentScreenState extends ConsumerState<CommentScreen> {
               flex: 2,
               child: InputField(
                 state: InputFieldState(
-                  controller: commentController,
+                  controller: _commentController,
                   label: ResCommentScreen.commentHere,
                   expands: true,
                 ),
@@ -102,24 +95,10 @@ class _CommentScreenState extends ConsumerState<CommentScreen> {
             Align(
               alignment: Alignment.centerRight,
               child: AppButton(
+                isLoading: _isLoading,
                 text: ResCommentScreen.leaveComment,
                 backgroundColor: theme.colorScheme.primary,
-                onPressed: () {
-                  if (emailController.text.isNotEmpty &&
-                      nameController.text.isNotEmpty &&
-                      commentController.text.isNotEmpty) {
-                    addComment(nameController.text ,emailController.text,
-                        commentController.text);
-                  }
-
-                  if(repo.authenticated){
-                    commentController.clear();
-                  }else{
-                    emailController.clear();
-                    nameController.clear();
-                    commentController.clear();
-                  }
-                },
+                onPressed: _addComment,
               ),
             ),
             const Spacer(),
@@ -129,20 +108,45 @@ class _CommentScreenState extends ConsumerState<CommentScreen> {
       ),
     );
   }
-  void addComment (String name, String email, String comment) {
-    databaseRef.child(nameController.text).push().set({
-      'name': name,
-      'email': email,
-      'comment': comment,
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _commentController.dispose();
+    super.dispose();
+  }
+
+  _addComment() async {
+    if (_emailController.text.isEmpty ||
+        _nameController.text.isEmpty ||
+        _commentController.text.isEmpty) {
+      context.showSnackMessage('Kindly fill all fields');
+    } else {
+      setState(() {
+        _isLoading = true;
+      });
+      final repo = ref.read(commentRepository);
+      repo
+          .addComment(
+            name: _nameController.text,
+            email: _emailController.text,
+            comment: _commentController.text,
+          )
+          .then((_) => _commentSubmitted());
+    }
+  }
+
+  _commentSubmitted() {
+    setState(() {
+      _isLoading = false;
     });
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        duration: const Duration(seconds: 2),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        content: const Text('Comment Submitted')
-      ),
-    );
-
+    context.showSnackMessage('Comment Submitted');
+    _commentController.clear();
+    final repo = ref.read(userRepository.notifier);
+    if (!repo.authenticated) {
+      _nameController.clear();
+      _emailController.clear();
+    }
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../components/copyright.dart';
-import '../components/spacers.dart';
 import '../data/repository/user_repository.dart';
-import '../utils/designs/assets.dart';
-import '../utils/designs/dimens.dart';
 import '../utils/designs/routes.dart';
 
 class SplashScreen extends ConsumerWidget {
